ed38876b81a747550faad8283dc9cc9706b189a3
Ana Beatriz Neri <ananeri32@hotmail.com>
2021-06-22 20:55:41 -0300

    model class
    Signed-off-by: Ana Beatriz Neri <ananeri32@hotmail.com>

---
+++ src/main/java/com/learnwiremock/dto/Movie.java
@@ -0,0 +1,20 @@
+package com.learnwiremock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    public String cast;
+    public Long movie_id;
+    public String name;
+    public LocalDate release_date;
+    public Integer year;
+
+}
